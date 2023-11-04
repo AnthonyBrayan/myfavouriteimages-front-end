7957c318a4562840cc7ddeca43f08c1e87f80966
Anthony Brayan <brayan.farronan@gmail.com>
2023-11-04 19:06:50 +0100

    Logo and views with space

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,38 +1,35 @@
-// import { Link } from 'react-router-dom';
-
-// const Navbar = () => {
-//     return (
-//       <nav>
-//       <Link to="/">Home</Link>
-//       <Link to="/addimage">Add Image</Link>
-//     </nav>
-//     );
-//    };
-//    export default Navbar;
-
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "../Navbar/Navbar.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function ColorSchemesExample() {
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="/">
-          <img
-              src="https://res.cloudinary.com/dipahj9kx/image/upload/v1699117341/Images/sjuobod1jrd4nk3u0tza.png"
-              width="70"
-              height="70"
-              className="d-inline-block align-top"
-              alt="Logo"
-            />
-          </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/" className="fs-5">My gallery</Nav.Link>
-            <Nav.Link href="/addimage" className="fs-5">Add image</Nav.Link>
-          </Nav>
+          <div>
+            <Navbar.Brand href="/">
+              <img
+                src="https://res.cloudinary.com/dipahj9kx/image/upload/v1699117341/Images/sjuobod1jrd4nk3u0tza.png"
+                width="70"
+                height="70"
+                className="d-inline-block align-top"
+                alt="Logo"
+              />
+            </Navbar.Brand>
+          </div>
+          <div>
+            <Nav className="me-auto">
+              <Nav.Link href="/" className="fs-5 navbar-link">
+                My gallery
+              </Nav.Link>
+              <Nav.Link href="/addimage" className="fs-5 navbar-link">
+                Add image
+              </Nav.Link>
+            </Nav>
+          </div>
         </Container>
       </Navbar>
     </>
